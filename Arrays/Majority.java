90910b923fdd635465a8ecf88b44f92395b133b0
Soma Karthikesh <karthikes004h@gmail.com>
2025-06-22 19:30:32 +0530

    Update and rename Main.java to Majority.java

--- Arrays/Majority.java
+++ Arrays/Majority.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Majority {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements in the array: ");
