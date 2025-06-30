13fb4d7af9bbb95f015192fce280120956c5c28e
Soma Karthikesh <karthikes004h@gmail.com>
2025-06-30 23:25:51 +0530

    Create  Diamond.java

---
+++ Patterns/Diamond.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // Top half
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Bottom half
+        for (int i = n - 2; i >= 0; i--)
