647afa708079e9017b79b85395af69f3eb7454e7
Soma Karthikesh <karthikes004h@gmail.com>
2025-07-12 21:39:42 +0530

    Create Product of Each even Array elements.java

---
+++ Arrays/Product of Each even Array elements.java
@@ -0,0 +1,41 @@
+// You are given an array of integers. For every even number in the array, compute the product of its digits and print them in order.
+// If no even numbers exist, print "No Even Elements".
+// Sample Input 0
+// 5
+// 122 91 174 99 1204
+// Sample Output 0
+// 4 28 0
+
+import java.util.Scanner;
+
+public class EvenDigitProduct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        boolean found = false;
+
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] % 2 == 0) {
+                int num = Math.abs(a[i]);
+                int prod = 1;
+                if (num == 0) prod = 0;
+                while (num > 0) {
+                    prod *= (num % 10);
+                    num /= 10;
+                }
+                System.out.print(prod + " ");
+                found = true;
+            }
+        }
+
+        if (!found)
+            System.out.println("No Even Elements");
+
+        sc.close();
+    }
+}
