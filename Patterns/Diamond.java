6fa207e9eb6f1a85803151d2c68c09e1e50e5deb
Soma Karthikesh <karthikes004h@gmail.com>
2025-07-24 07:22:41 +0530

    Update Diamond.java

--- Patterns/Diamond.java
+++ Patterns/Diamond.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class DiamondPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
 
         // Top half
         for (int i = 0; i < n; i++) {
+            // Spaces
             for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
+            // Stars
             for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
             }
@@ -17,4 +20,16 @@ public class DiamondPattern {
         }
 
         // Bottom half
-        for (int i = n - 2; i >= 0; i--)
+        for (int i = n - 2; i >= 0; i--) {
+            // Spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // Stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
